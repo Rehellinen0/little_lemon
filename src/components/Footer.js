adf3b89d0f38adfa952ba9e4cee5a00d1ec23d3a
Yuanke <yuankemiao.dev@gmail.com>
2023-02-21 16:19:00 +0200

    some meta tags and OGP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 const Footer = () => {
-
+    <>
+        <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="reservation">Reservation</a></li>
+                <li><a href="#order">Order Online</a></li>
+                <li><a href="login">Login</a></li>
+            </ul>
+    </>
 };
 
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
+import logo from "../images/logo.svg"
+
 const Nav = () => {
     return(
         <>
-            <img src="src/images/Logo.svg"/>
+            <img src={logo} alt="Logo" />
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="reservation">Reservation</a></li>
+                <li><a href="#order">Order Online</a></li>
+                <li><a href="login">Login</a></li>
+            </ul>
         </>
     )
 };
